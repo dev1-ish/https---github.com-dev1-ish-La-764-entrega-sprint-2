bd8ca928256ebefaab89e645af6d9d797157d7e0
dev1-ish <alma.miguez30@gmail.com>
2022-06-19 01:48:06 -0300

    [update] new design, enter keyboard, validation

--- index.js
+++ index.js
@@ -3,14 +3,77 @@ const inputCost = document.getElementById("cost");
 const submitButton = document.getElementById("submit");
 const listaNombreYmonto = document.querySelector("#nombreYmonto");
 
+
+
+// trigger submit button with Enter keyboard
+
+// inputName.addEventListener("keypress", function (event) {
+//     if (event.key === "Enter") {
+//         submitButton.click();
+//     }
+// })
+// inputCost.addEventListener("keypress", function (event) {
+//     if (event.key === "Enter") {
+//         submitButton.click();
+//     }
+// })
+
+function submitWithEnter(input) {
+    input.addEventListener("keypress", event => {
+        (event.key === "Enter");
+    })
+}
+
+const submitInputs = function (submitWithEnter){
+    submitWithEnter(inputName);
+    submitWithEnter(inputCost);
+}
+
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})
+
+
+
+// si hubiera checkbox
+
+// function isCash() {
+    
+// }
+
+
+
+
 let peopleName = "";
 let cost = 0;
+
+// 1- PREGUNTAR TOMA DE INPUTS CAMBIANDO LOS SIG. EVENTOS:
 
 inputName.addEventListener("change", (evt) => catchName(evt));
 inputCost.addEventListener("change", (evt) => catchCost(evt));
 
 const catchName = (evt) => (peopleName = evt.target.value);
 const catchCost = (evt) => (cost = evt.target.value);
+
+// 2- modificar/ ELIMINAR F(X) -> HECHA LA VALIDACIÓN
 
 const ejecutar = () => {
   if (cost >= 1) {
@@ -24,11 +87,13 @@ const ejecutar = () => {
 };
 
 
+
 const nameArray = []
 const costArray = []
 const cardSumData = document.getElementById("card-text-sum")
 const cardAvgData = document.getElementById("card-text-avg")
 
+// 3- REVISAR/MODIFICAR F(X)
 
 function mostrarNombreYmonto(_nombre, _monto) {
   nameArray.push(_nombre)
@@ -37,6 +102,7 @@ function mostrarNombreYmonto(_nombre, _monto) {
 }
 
 
+// 4- SACAR BOTONES P/TOTAL, REALIZAR CÁLCULO DINÁMICO
 
 const initialValue = 0;
 
